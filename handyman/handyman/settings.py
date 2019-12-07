40fd9fe5f914fc657fa71665327d02499f42d2fd
brian13th <brian13th13@gmail.com>
2019-12-07 04:09:04 +0200

    setting secret key better

--- handyman/handyman/settings.py
+++ handyman/handyman/settings.py
@@ -15,13 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-_SECRET_KEY_ = os.environ['SECRET_KEY']
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '8#+$t=51oqji^%sp@_h@apc%hxgoo4ppe2w%7oxr*yv+7d0m(u'
-SECRET_KEY = _SECRET_KEY_
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
