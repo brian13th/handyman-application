74ba17eb8f73593c3843fd3b2fc6b06fd9fbeed0
brian13th <brian13th13@gmail.com>
2019-12-10 01:06:35 +0200

    implement detail view for company posts

--- handyman/clients/urls.py
+++ handyman/clients/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ClientPostListView
+from .views import ClientPostListView, ClienPostDetailView
 
 
 urlpatterns = [
-    path('client-home/', ClientPostListView.as_view(template_name='clients/clienthome.html'), name='client-home')
+    path('client-home/', ClientPostListView.as_view(template_name='clients/clienthome.html'), name='client-home'),
+    path('client-home/<pk>/', ClienPostDetailView.as_view(template_name='companies/companypost.html'), name='company-post'),
 ]
--- handyman/clients/views.py
+++ handyman/clients/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import ClientPost
 from django.apps import apps
 
 companypost = apps.get_model('companies', 'CompanyPost')
 
 class ClientPostListView(ListView):
+    model = companypost
+
+class ClienPostDetailView(DetailView):
     model = companypost
